89f75e04c2fe2eb01afbc70e1e4f34b9e93e83e9
Martino Sorbaro <martino.sorbaro@posteo.net>
2022-06-16 14:45:06 +0200

    file for linear train

---
+++ linear_train.py
@@ -0,0 +1,63 @@
+from model import LPLVGG11
+import torch
+from torch import nn
+from lpl import LPLPass
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import ToTensor
+
+
+# model = LPLVGG11()
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(3072, 500),
+    # nn.ReLU(),
+    # LPLPass(),
+)
+model.load_state_dict(torch.load("models/single_layer_noRelu_noPred.pth"))
+
+cifar_ds = CIFAR10(root='../datasets/', transform=ToTensor(), train=True)
+cifar_ds_test = CIFAR10(root='../datasets/', transform=ToTensor(), train=False)
+dl = torch.utils.data.DataLoader(cifar_ds, batch_size=800)
+dl_test = torch.utils.data.DataLoader(cifar_ds_test, batch_size=800)
+
+
+linear = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(500, 10).cuda()
+)
+optimizer = torch.optim.Adam(linear.parameters(), lr=1e-3)
+model.cuda()
+
+criterion = torch.nn.CrossEntropyLoss()
+for epoch in range(10):
+    print(f"EPOCH {epoch}")
+
+    for images, labels in dl:
+        images = images.cuda()
+        labels = labels.cuda()
+        representation = model(images)
+
+        out = linear(representation)
+        loss = criterion(out, labels)
+        _, pred = torch.max(out, axis=1)
+        acc = (pred == labels).float().mean().item()
+        print("Batch accuracy:", acc)
+        print("Loss", loss.item())
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    accs = []
+    for images, labels in dl_test:
+        images = images.cuda()
+        labels = labels.cuda()
+        representation = model(images)
+
+        out = linear(representation)
+        _, pred = torch.max(out, axis=1)
+        acc = (pred == labels).float().mean().item()
+        accs.append(acc)
+    print("Acc", sum(accs)/len(accs))
+
+
